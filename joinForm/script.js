3a4da3ed5bd61fd29376bc7dd13fda8885b073c1
HYNIC <edocnuyh@gmail.com>
2022-03-20 19:52:21 +0900

    joinForm ver1.1

--- joinForm/script.js
+++ joinForm/script.js
@@ -1,46 +1,64 @@
-// 연락처 포커스 이동
-function moveFocus(arg, next, len) {
-    if(arg.value.length == len) {
-        next.focus();
-        return;
+// 변수 선언
+let joinBtn, email, username, pw, re_pw, phone1, phone2, phone3, city, gender, input_err, email_err, pw_err;
+
+joinBtn = document.querySelector("#submit-btn");
+email = document.querySelector("#email");
+username = document.querySelector("#name");
+pw = document.querySelector("#pw");
+re_pw = document.querySelector("#pw2");
+phoneNum = document.querySelectorAll(".phone");
+phone1 = phoneNum[0];
+phone2 = phoneNum[1];
+phone3 = phoneNum[2];
+city = document.querySelector(".join-form-city"); 
+
+// 이벤트리스너 
+document.querySelector(".join-form").addEventListener("input", activateBtn); // 폼 이벤트 리스너(버튼활성화)
+joinBtn.addEventListener("click", confirmInput); // 버튼클릭시 가입정보 alert띄우기
+email.addEventListener("input", checkEmail); // 이메일 형식 체크
+
+// 숫자입력체크
+phoneNum.forEach( (phone) => {
+    phone.addEventListener("input", (e) => checkNum(e.currentTarget));
+});
+
+// 연락처 포커스이동
+for (let i = 0; i < phoneNum.length; i++) {
+    if (i < phoneNum.length -1) {
+        phoneNum[i].addEventListener("keyup", (e) => moveFocus(e.currentTarget, phoneNum[i + 1]));
+    } else {
+        phoneNum[i].addEventListener("keyup", (e) => moveFocus(e.currentTarget, city));
     }
 }
 
+
 // 버튼 활성화
-function activateBtn() {            
-    const joinBtn = document.querySelector("#submit-btn"),
-        email = document.querySelector("#email"),
-        name = document.querySelector("#name"),
-        pw = document.querySelector("#pw"),
-        re_pw = document.querySelector("#pw2"),
-        phone1 = document.querySelector("#phone1"),
-        phone2 = document.querySelector("#phone2"),
-        phone3 = document.querySelector("#phone3"),
-        city = document.querySelector(".join-form-city"),
-        gender = document.querySelector("input[name='gender']:checked");
-    
-    if (!(email.value && name.value && pw.value && re_pw.value && (phone1.value.length == 3) && (phone2.value.length == 4) && (phone3.value.length == 4) && (city.value.length != 0) && gender.value)) {
+function activateBtn() {
+    checkPw();
+    // input마다 gender checked된 값 확인
+    gender = document.querySelector("input[name='gender']:checked");
+
+    // 모든 값이 작성되었는지 확인
+    let input_check = email.value && username.value && pw.value && re_pw.value && (phone1.value.length == 3) && (phone2.value.length == 4) && (phone3.value.length == 4) && (city.value.length != 0) && gender.value;
+
+    // 에러메시지 떠있는지 확인
+    let isErr = (email_err.style.display == "block") || (pw_err.style.display == "block");
+
+    if (!input_check) {
+        joinBtn.disabled = true;
+    } else if (isErr) {
         joinBtn.disabled = true;
     } else {
         joinBtn.disabled = false;
     }
 }
 
+// 가입정보 alert창 띄우는 함수
 function confirmInput() {
-    let email = document.querySelector("#email"),
-        name = document.querySelector("#name"),
-        pw = document.querySelector("#pw"),
-        re_pw = document.querySelector("#pw2"),
-        phone1 = document.querySelector("#phone1"),
-        phone2 = document.querySelector("#phone2"),
-        phone3 = document.querySelector("#phone3"),
-        city = document.querySelector(".join-form-city"),
-        gender = document.querySelector("input[name='gender']:checked");
-
     let msg;
     msg = '가입정보를 확인해 주세요\n'
     msg += '\nE-mail : ' + email.value;
-    msg += '\n이름 : ' + name.value;
+    msg += '\n이름 : ' + username.value;
     msg += '\n비밀번호 : ' + pw.value;
     msg += '\n비밀번호 확인 : ' + re_pw.value;
     msg += '\n연락처 : ' + phone1.value + '-' + phone2.value + '-' + phone3.value;
@@ -50,40 +68,56 @@ function confirmInput() {
     alert(msg);
 }
 
+// email형식 체크 함수
 function checkEmail() {
-    let email = document.querySelector("#email");
-    let errMsg = document.querySelector("#err-email");
+    email_err = document.querySelector("#err-email");
 
-    let str = email.value.split('@');
+    let email_str = email.value.split('@');
     
-    if (!str[1]) {
+    if (!email_str[1]) {
         email.style.border = "2px solid tomato";
-        errMsg.style.display = "block";
-        errMsg.innerText = "올바른 이메일 형식이 아닙니다"
+        email_err.style.display = "block";
+        email_err.innerText = "올바른 이메일 형식이 아닙니다"
     } else {
-        email.style.border = "1px solid grey";
-        errMsg.style.display = "none";
-        errMsg.innerText = "";
-    }
+        let email_domain = email_str[1].split('.');
+        if (!email_domain[1]) {
+            email.style.border = "2px solid tomato";
+            email_err.style.display = "block";
+            email_err.innerText = "올바른 이메일 형식이 아닙니다"
+        } else {
+            email.style.border = "1px solid grey";
+            email_err.style.display = "none";
+            email_err.innerText = "";
+        }
+    } 
 }
 
+// 비밀번호 일치 확인
 function checkPw() {
-    let pw = document.querySelector("#pw");
-    let re_pw = document.querySelector("#pw2");
-    let errMsg = document.querySelector("#err-pw");
+    pw_err = document.querySelector("#err-pw");
 
     if (pw.value != re_pw.value) {
         pw.style.border = "2px solid tomato";
         re_pw.style.border = "2px solid tomato";
-        errMsg.style.display = "block";
-        errMsg.innerText = "비밀번호가 일치하지 않습니다."
+        pw_err.style.display = "block";
+        pw_err.innerText = "비밀번호가 일치하지 않습니다."
     } else {
         pw.style.border = "1px solid grey";
         re_pw.style.border = "1px solid grey";
-        errMsg.style.display = "none";
-        errMsg.innerText = "";
+        pw_err.style.display = "none";
+        pw_err.innerText = "";
     }
 }
 
+// 연락처 숫자만 입력가능하도록.
+function checkNum(arg) {
+    arg.value = arg.value.replace(/[^0-9]/g,'');
+}
 
-
+// 연락처 포커스 이동
+function moveFocus(arg, next) {
+    if(arg.value.length == arg.getAttribute('maxlength')) {
+        next.focus();
+        return;
+    }
+}
